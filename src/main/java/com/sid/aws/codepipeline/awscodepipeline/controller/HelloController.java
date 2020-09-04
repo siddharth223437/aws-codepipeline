5571cc7a1caaa0515a6637cc29bfa39eaa4eefd0
siddharth223437 <siddharthsarathe.nmims@gmail.com>
2020-09-04 16:10:54 -0500

    hello controller

---
+++ src/main/java/com/sid/aws/codepipeline/awscodepipeline/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sid.aws.codepipeline.awscodepipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getName(){
+        return "siddharth";
+    }
+}
