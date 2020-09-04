3ed0c17cbd253f2d686f7a53e520dfbf77a0831e
siddharth223437 <siddharthsarathe.nmims@gmail.com>
2020-09-04 16:08:46 -0500

    Initial commit

---
+++ src/main/java/com/sid/aws/codepipeline/awscodepipeline/AwsCodepipelineApplication.java
@@ -0,0 +1,13 @@
+package com.sid.aws.codepipeline.awscodepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodepipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodepipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sid/aws/codepipeline/awscodepipeline/AwsCodepipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.aws.codepipeline.awscodepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodepipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
